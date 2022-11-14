ab9b5f4e1c5d8145f0079911f8c155fc9e42bb9d
Sean-Kurian <Sean-Kurian@users.noreply.github.com>
2022-11-14 01:46:23 -0500

    added unit tests

--- Education_Pathways/tests/test_app.py
+++ Education_Pathways/tests/test_app.py
@@ -1,8 +1,10 @@
 from app import app
+import sqlite3
+import os
 from minor import check_course_in_minor
 from flask.testing import FlaskClient
-
-
+from sqlite_config import create_connection, select_all_keywords_for_course, select_all_sessions, select_all_instructors
+from sqlite3 import Error, connect
 
 def test_check_course_in_minor():
     course = "MIE439H1S"
@@ -81,3 +83,24 @@ def test_user_wishlist_minorCheck_endpoint():
 
     assert response.status_code == 200
 
+def test_select_all_keywords_for_course():
+    database = r"ep_database"
+
+    # create a database connection
+    conn = create_connection(database)
+    assert(select_all_keywords_for_course(conn, 'ECE444') == ['Software Engineer', 'Full Stack Developer', 'Database Administrator', 'Site Reliability Engineer', 'DevOps Engineer'])
+
+def test_select_all_sessions():
+    database = r"ep_database"
+    
+    conn = create_connection(database)
+
+    assert(select_all_sessions(conn) == [(1, 2022, None, 1, 'ECE444', 1, 0, 1), (2, 2022, None, 1, 'CSC384', 1, 0, 1), (3, 2022, None, 1, 'ECE496', 3, 0, 1), (4, 2022, None, 3, 'ECE496', 1, 0, 1), (5, 2022, None, 1, 'PHL233', 1, 0, 1), (6, 2022, None, 1, 'POL101', 1, 0, 0), (7, 2022, None, 2, 'ECE318', 1, 0, 1), (8, 2022, None, 2, 'ECE368', 1, 0, 1), (9, 2022, None, 2, 'ECE568', 1, 0, 1), (10, 2022, None, 2, 'TEP445', 1, 0, 1), (11, 2022, None, 2, 'ECE311', 1, 0, 1), (12, 2022, None, 2, 
+'ECE344', 1, 0, 1), (13, 2022, None, 2, 'ECE421', 1, 0, 1), (14, 2022, None, 2, 'ECE472', 1, 0, 1), (15, 2022, None, 1, 'ECE302', 1, 0, 0), (16, 2022, None, 1, 'ECE326', 1, 0, 1), (17, 2022, None, 1, 'ECE345', 1, 0, 1), (18, 2022, None, 1, 'ECE361', 1, 0, 1), (19, 2022, None, 1, 'APS360', 1, 0, 0), (20, 2022, None, 1, 'TEP444', 1, 0, 1)])
+
+def test_select_all_instructors():
+    database = r"ep_database"
+ 
+    conn = create_connection(database)
+
+    assert(select_all_instructors(conn) == [(1, 'Shurui Zhou', 'shuruiz@ece.utoronto.ca'), (2, 'Steve Engels', 'csc384-2022-09@cs.toronto.edu'), (3, 'Courtney Jung', 'courtney.jung@utoronto.ca'), (4, 'Imogen Dickie', 'imogen.dickie@utoronto.ca'), (5, 'Belinda Wang', 'belinda.wang@utoronto.ca'), (6, 'Khoman Phang', 'khoman.phang@utoronto.ca'), (7, 'Bruno Korst', 'bkf@ece.utoronto.ca')])
