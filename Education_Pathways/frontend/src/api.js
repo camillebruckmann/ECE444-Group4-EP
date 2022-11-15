1ec98855eb53c0c163e45d968a9ca35490758539
Sean-Kurian <Sean-Kurian@users.noreply.github.com>
2022-11-15 16:39:04 -0500

    testing api endpoint change

--- Education_Pathways/frontend/src/api.js
+++ Education_Pathways/frontend/src/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   //baseURL: "https://lab3-docker.herokuapp.com/"
-  baseURL: "https://lab3-docker.herokuapp.com/"
+  baseURL: "https://ep4sight.herokuapp.com/"
 });
