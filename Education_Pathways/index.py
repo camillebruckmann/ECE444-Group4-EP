66b9e2815056f25b744d2d665eb6a7fa859bd95e
Sean-Kurian <Sean-Kurian@users.noreply.github.com>
2022-11-15 17:46:30 -0500

    removed hardcoded prereqs2

--- Education_Pathways/index.py
+++ Education_Pathways/index.py
@@ -44,7 +44,7 @@ def search_course_by_code(s):
             'code': d['Code'],
             'name': d['Name'],
             'description': d['Course Description'],
-            'prereq': ['APS101H1, ECE101H1'],
+            'prereq': ['Pre-requisites'],
             'coreq': ['APS102H1, ECE102H1'],
             'exclusion': ['APS102H1, ECE102H1'],
             'division': d['Division'],
